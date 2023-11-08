584a029a1331f677ff76447b53abb9c49c158a86
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2023-11-08 15:10:41 +0100

    commit test with operator '+'

--- solution.js
+++ solution.js
@@ -1,5 +1,17 @@
 function getSolution(rpnExpression) {
-  return [Number(rpnExpression)];
+  stack = [];
+  rpnExpressionSplited = rpnExpression.split(" ");
+  rpnExpressionSplited.forEach((value) => {
+    const isNum = /^\d+$/.test(value);
+    if (isNum) {
+      stack.push(Number(value));
+    } else if (value === "+") {
+      const num1 = stack.pop();
+      const num2 = stack.pop();
+      stack.push(num1 + num2);
+    }
+  });
+  return stack[0];
 }
 
 module.exports = getSolution;
--- solution.test.js
+++ solution.test.js
@@ -3,6 +3,11 @@ const getSolution = require("./solution");
 describe("getting solutions", () => {
   test("it parses single number", () => {
     const res = getSolution("42");
-    expect(res).toEqual([42]);
+    expect(res).toEqual(42);
+  });
+
+  test("it parses single number", () => {
+    const res = getSolution("42 20 +");
+    expect(res).toEqual(62);
   });
 });

090f959af7b01a1345b93aa063920c5d005bed85
Kylian Sauvée <kylian@thiqa.fr>
2023-11-08 16:44:05 +0100

    Refactor solution

--- solution.js
+++ solution.js
@@ -1,41 +1,43 @@
 function getSolution(rpnExpression) {
-  if (rpnExpression == " ") {
-    throw TypeError("Empty string");
-  }
-  stack = [];
-  rpnExpressionSplited = rpnExpression.split(" ");
-  rpnExpressionSplited.forEach((value) => {
-    const isNumOrDecimal = /^\d*\.?\d+$/.test(value);
-    if (isNumOrDecimal) {
-      stack.push(Number(value));
-    } else if (value === "sqrt") {
-      const num1 = stack.pop();
-      stack.push(Math.sqrt(num1));
-    } else if (stack.length < 2) {
-      throw TypeError("Not enough operands");
-    } else if (value === "+") {
-      const num1 = stack.pop();
-      const num2 = stack.pop();
-      stack.push(num2 + num1);
-    } else if (value === "-") {
-      const num1 = stack.pop();
-      const num2 = stack.pop();
-      stack.push(num2 - num1);
-    } else if (value === "*") {
-      const num1 = stack.pop();
-      const num2 = stack.pop();
-      stack.push(num2 * num1);
-    } else if (value === "/") {
-      const num1 = stack.pop();
-      const num2 = stack.pop();
-      stack.push(num2 / num1);
-    } else if (value === "max") {
-      stackCopy = stack.slice();
-      stack = [];
-      stack.push(Math.max(...stackCopy));
+    if (rpnExpression == " ") {
+        throw TypeError("Empty string");
     }
-  });
-  return stack[0];
+    stack = [];
+    rpnExpressionSplited = rpnExpression.split(" ");
+    rpnExpressionSplited.forEach((token) => {
+        const isNumOrDecimal = /^\d*\.?\d+$/.test(token);
+        if (isNumOrDecimal) {
+            stack.push(Number(token));
+        } else if (token === "sqrt") {
+            const num1 = stack.pop();
+            stack.push(Math.sqrt(num1));
+        } else if (token === "max") {
+            stackCopy = stack.slice();
+            stack = [];
+            stack.push(Math.max(...stackCopy));
+        } else if (stack.length < 2) {
+            throw TypeError("Not enough operands");
+        } else if (token === "+") {
+            const num1 = stack.pop();
+            const num2 = stack.pop();
+            stack.push(num2 + num1);
+        } else if (token === "-") {
+            const num1 = stack.pop();
+            const num2 = stack.pop();
+            stack.push(num2 - num1);
+        } else if (token === "*") {
+            const num1 = stack.pop();
+            const num2 = stack.pop();
+            stack.push(num2 * num1);
+        } else if (token === "/") {
+            const num1 = stack.pop();
+            const num2 = stack.pop();
+            stack.push(num2 / num1);
+        } else {
+            throw TypeError("Unknown token");
+        }
+    });
+    return stack.pop();
 }
 
 module.exports = getSolution;
